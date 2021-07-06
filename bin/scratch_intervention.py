19b7ab5500094d8f8d7c842416698ea070876790
Michael Chang <mbchang2017@gmail.com>
2021-07-06 09:43:52 -0700

    clean up

--- bin/scratch_intervention.py
+++ bin/scratch_intervention.py
@@ -52,34 +52,10 @@ def force_intervention():
     for t in range(T):
         print('t', t)
 
-        # if t == T // 2:
-        #     print('INTERVENE')
-        #     act_n = []
-        #     agent_index = np.random.randint(len(policies))
-        #     for i, policy in enumerate(policies):
-        #         if i == agent_index:
-        #             action = policy.action(obs_n[i])
-        #         else:
-        #             action = policy.do_nothing()
-        #         act_n.append(action)
-        # else:
-        #     # query for action from each agent's policy
-        #     act_n = []
-        #     for i, policy in enumerate(policies):
-        #         action = policy.do_nothing()
-        #         act_n.append(action)
-
-        # # step environment
-        # obs_n, reward_n, done_n, _ = env.step(act_n)
-        # print('Obs: {} Act: {} Rew: {}'.format(obs_n, act_n, reward_n))
-
-
         if t == t_intervene:
             obs_n, act_n, reward_n, done_n = mr.random_intervention_episode_step(obs_n, env, policies, verbose=False)
         else:
             obs_n, act_n, reward_n, done_n = mr.do_nothing_episode_step(obs_n, env, policies, verbose=False)
-
-
 
         # render all agent views
         env.render()
