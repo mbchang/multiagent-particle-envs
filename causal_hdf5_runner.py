a959f739fe915e9116852050fc36f96525a75806
Michael Chang <mbchang2017@gmail.com>
2021-11-08 17:33:42 -0800

    added to causal_hdf5_runner.py

--- causal_hdf5_runner.py
+++ causal_hdf5_runner.py
@@ -405,6 +405,21 @@ def whiteball_push_geb_9_27_2021():
     r.add_flag('data_root', ['singlewhiteballpush'])
     r.generate_commands(execute=args.for_real)
 
+
+def whiteball_push_gauss1_10_25_2021():
+    """
+        t = 20
+    """
+    r = RunnerWithIDs(command='python bin/counterfactual_hdf5.py --scenario intervenable_bouncing_white_action.py', gpus=[])
+    r.add_flag('num_episodes', ['2000'])
+    r.add_flag('max_episode_length', ['10'])
+    r.add_flag('t_intervene', ['0'])
+    r.add_flag('num_entities', ['4'])
+    r.add_flag('color_dist', ['uniform_k20'])
+    r.add_flag('intervention_type', ['displacement'])
+    r.add_flag('data_root', ['singlewhiteballpush'])
+    r.generate_commands(execute=args.for_real)
+
 if __name__ == '__main__':
     # all_counterfactuals_draft1_7_6_2021()
     # all_counterfactuals_geb_7_6_2021()
@@ -423,4 +438,5 @@ if __name__ == '__main__':
     # whiteball_push_baobab_9_24_2021()
     # whiteball_push_geb_9_24_2021()
     # whiteball_push_baobab_9_27_2021()
-    whiteball_push_geb_9_27_2021()
+    # whiteball_push_geb_9_27_2021()
+    whiteball_push_gauss1_10_25_2021()
